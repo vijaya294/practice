da862f6c27df98ca569ea8f6818fb555a5d39510
Srilatha <koduri0311@gmail.com>
2024-11-10 21:09:33 +0000

    shell

--- 01.install-mysql-shell.sh
+++ 01.install-mysql-shell.sh
@@ -2,12 +2,24 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access"
     exit 1
 fi
 
-dnf install git -y
+dnf list installed git
+
+VALIDATE $? "Listing Git"
 
 
